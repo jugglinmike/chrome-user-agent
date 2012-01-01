f4ce51dc0262f95fe0e60c4113bc52b9800ed646
Mike <mike@mikepennisi.com>
2012-01-01 09:48:42 -0500

    Move background page JavaScript into external JS file

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.experimental.webRequest.onBeforeSendHeaders.addListener(function(details) {
+	var headers = details.requestHeaders;
+	if( !localStorage['user-agent'] ) {
+		return;
+	}
+	for(var i = 0, l = headers.length; i < l; ++i) {
+		if( headers[i].name == 'User-Agent' ) {
+			break;
+		}
+	}
+	if(i < headers.length) {
+		headers[i].value = localStorage['user-agent'];
+	}
+	return {requestHeaders: headers};
+}, undefined, ['requestHeaders','blocking']);
